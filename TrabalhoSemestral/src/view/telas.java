400699087e8a4b545f48836504305da36d5430b3
Luigi Lucas <121830830+Luigi-Lucas@users.noreply.github.com>
2023-06-05 23:16:50 -0300

    Initial commit

--- TrabalhoSemestral/src/view/telas.java
+++ TrabalhoSemestral/src/view/telas.java
@@ -363,21 +363,4 @@ public class telas extends JFrame {
 		btnExcluir.addActionListener(aC);
 		btnExcluir_1.addActionListener(aC);
 	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}
 }
